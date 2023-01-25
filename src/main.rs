706954d08506c045b283b9b1e390b2ad1dd4b3fa
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-01-25 12:34:23 +0100

    0.1.0

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use image::open;
+use rayon::prelude::*;
+use std::time::Instant;
+
+fn multi_threaded() -> u128 {
+    let now = Instant::now();
+    ["1.webp", "2.webp", "3.webp"].par_iter().for_each(|i| {
+        let input_path = format!("input/{i}");
+        let mut image = open(input_path).unwrap().into_rgb8();
+        let (width, height) = image.dimensions();
+        (0..width).for_each(|x| {
+            (0..height).for_each(|y| {
+                let pixel = image.get_pixel_mut(x, y);
+                let image::Rgb(data) = *pixel;
+                let average = data[0] / 3 + data[1] / 3 + data[2] / 3;
+                *pixel = image::Rgb([average, average, average]);
+            })
+        });
+        let output_path = format!("output-multi/{i}");
+        image.save(output_path).unwrap();
+    });
+    now.elapsed().as_millis()
+}
+
+fn single_threaded() -> u128 {
+    let now = Instant::now();
+    ["1.webp", "2.webp", "3.webp"].iter().for_each(|i| {
+        let input_path = format!("input/{i}");
+        let mut image = open(input_path).unwrap().into_rgb8();
+        let (width, height) = image.dimensions();
+        (0..width).for_each(|x| {
+            (0..height).for_each(|y| {
+                let pixel = image.get_pixel_mut(x, y);
+                let image::Rgb(data) = *pixel;
+                let average = data[0] / 3 + data[1] / 3 + data[2] / 3;
+                *pixel = image::Rgb([average, average, average]);
+            })
+        });
+        let output_path = format!("output-single/{i}");
+        image.save(output_path).unwrap();
+    });
+    now.elapsed().as_millis()
+}
+
+fn main() {
+    let single_threaded_time: u128 = (0..10).map(|_| single_threaded()).sum::<u128>() / 10;
+    println!("time for single threaded: {single_threaded_time}ms");
+    let multi_threaded_time: u128 = (0..10).map(|_| multi_threaded()).sum::<u128>() / 10;
+    println!("time for single threaded: {multi_threaded_time}ms");
+}
