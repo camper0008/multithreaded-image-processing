44e3e11a2b11aa9dcb73d546a339d6601c6cae26
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-01-25 15:15:38 +0100

    move repeated code into shared function

--- src/main.rs
+++ src/main.rs
@@ -2,43 +2,35 @@ use image::open;
 use rayon::prelude::*;
 use std::time::Instant;
 
+fn process_image(image_name: &&str) {
+    let input_path = format!("input/{image_name}");
+    let mut image = open(input_path).unwrap().into_rgb8();
+    let (width, height) = image.dimensions();
+    (0..width).for_each(|x| {
+        (0..height).for_each(|y| {
+            let pixel = image.get_pixel_mut(x, y);
+            let image::Rgb(data) = *pixel;
+            let average = data[0] / 3 + data[1] / 3 + data[2] / 3;
+            *pixel = image::Rgb([average, average, average]);
+        })
+    });
+    let output_path = format!("output-multi/{image_name}");
+    image.save(output_path).unwrap();
+}
+
 fn multi_threaded() -> u128 {
     let now = Instant::now();
-    ["1.webp", "2.webp", "3.webp"].par_iter().for_each(|i| {
-        let input_path = format!("input/{i}");
-        let mut image = open(input_path).unwrap().into_rgb8();
-        let (width, height) = image.dimensions();
-        (0..width).for_each(|x| {
-            (0..height).for_each(|y| {
-                let pixel = image.get_pixel_mut(x, y);
-                let image::Rgb(data) = *pixel;
-                let average = data[0] / 3 + data[1] / 3 + data[2] / 3;
-                *pixel = image::Rgb([average, average, average]);
-            })
-        });
-        let output_path = format!("output-multi/{i}");
-        image.save(output_path).unwrap();
-    });
+    ["1.webp", "2.webp", "3.webp"]
+        .par_iter()
+        .for_each(process_image);
     now.elapsed().as_millis()
 }
 
 fn single_threaded() -> u128 {
     let now = Instant::now();
-    ["1.webp", "2.webp", "3.webp"].iter().for_each(|i| {
-        let input_path = format!("input/{i}");
-        let mut image = open(input_path).unwrap().into_rgb8();
-        let (width, height) = image.dimensions();
-        (0..width).for_each(|x| {
-            (0..height).for_each(|y| {
-                let pixel = image.get_pixel_mut(x, y);
-                let image::Rgb(data) = *pixel;
-                let average = data[0] / 3 + data[1] / 3 + data[2] / 3;
-                *pixel = image::Rgb([average, average, average]);
-            })
-        });
-        let output_path = format!("output-single/{i}");
-        image.save(output_path).unwrap();
-    });
+    ["1.webp", "2.webp", "3.webp"]
+        .iter()
+        .for_each(process_image);
     now.elapsed().as_millis()
 }
 
@@ -46,5 +38,5 @@ fn main() {
     let single_threaded_time: u128 = (0..10).map(|_| single_threaded()).sum::<u128>() / 10;
     println!("time for single threaded: {single_threaded_time}ms");
     let multi_threaded_time: u128 = (0..10).map(|_| multi_threaded()).sum::<u128>() / 10;
-    println!("time for single threaded: {multi_threaded_time}ms");
+    println!("time for multi threaded: {multi_threaded_time}ms");
 }
